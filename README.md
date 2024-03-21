7be4db09ecc209144ea331375a51bc4c7436bc29
LNTH <lenguyenthanhhuy94@gmail.com>
2024-03-22 01:58:59 +0700

    refactor: :art: correct function's name

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # TODOs
 
+- [ ] Read svelte actions
 - [ ] Remove DaisyUI, 
 - [ ] Change checkbox UI
 - [ ] Change Login page from home route to login route
-- [ ] Read svelte actions
 - [ ] Separate logic & UI
 - [ ] Implement Login page
 - [ ] Read svelte store
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,17 +1,15 @@
 export function generateUniqueId() {
-
     const timestamp = Date.now()
     const randomPart = Math.floor(Math.random() * 100000)
 
     return `id-${timestamp}-${randomPart}`
 }
 
-export function getToday() {
-
+export function getTodayDate() {
     const today = new Date()
     const year = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, '0')
     const day = String(today.getDate()).padStart(2, '0')
+
     return `${year}-${month}-${day}`;
-    
 }
